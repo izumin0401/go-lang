2241b4a18d94744d6b219fb9db2e02cdfa1fd0a2
kohei izumi <unitedbz0401@gmail.com>
2020-03-21 02:23:48 +0900

    add goroutine

---
+++ goroutine/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"time"
+)
+
+// スリープさせる
+func sleep(i int) {
+	log.Print(strconv.Itoa(i + 1) + "回目")
+
+	time.Sleep(1 * time.Second)
+}
+
+// メイン関数
+func main() {
+	const sleepSecondTime int = 3
+
+	log.Print("start")
+
+	for i := 0; i < sleepSecondTime; i ++ {
+        go sleep(i)  // ゴルーチン使用
+	}
+
+	time.Sleep(4 * time.Second)
+
+	log.Print("end")
+}
